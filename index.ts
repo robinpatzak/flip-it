a037814a5d8db48f87a614f2d89f63903e15c03a
Robin Patzak <robin.patzak@proton.me>
2025-01-02 22:33:00 +0100

    Add kicking a player

--- index.ts
+++ index.ts
@@ -1,47 +1,97 @@
 import express from "express";
-import { Server } from "socket.io";
-import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+import { createServer, ServerResponse } from "http";
 import { v4 as uuidv4 } from "uuid";
+
+interface Player {
+  playerName: string;
+  isHost: boolean;
+  socketId: string;
+}
+
+interface ServerToClientEvents {
+  roomCreated: (data: { roomId: string }) => void;
+  updatePlayers: (players: Player[]) => void;
+  kicked: (data: { playerName: string }) => void;
+}
+
+interface ClientToServerEvents {
+  createRoom: (data: { playerName: string; isHost: boolean }) => void;
+  joinRoom: (data: {
+    roomId: string;
+    playerName: string;
+    isHost: boolean;
+  }) => void;
+  kickPlayer: (data: { roomId: string; playerName: string }) => void;
+}
 
 const PORT = 4000;
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, {
+const io = new Server<ClientToServerEvents, ServerToClientEvents>(httpServer, {
   cors: { origin: "http://localhost:5173" },
 });
 
-const rooms = new Map();
+const rooms: Map<string, Player[]> = new Map();
 
-io.on("connection", (socket) => {
-  socket.on("createRoom", ({ playerName, isHost }) => {
-    const roomId = uuidv4();
-    rooms.set(roomId, [{ playerName, isHost }]);
-    socket.join(roomId);
-    io.to(roomId).emit("roomCreated", { roomId });
-    io.to(roomId).emit("updatePlayers", rooms.get(roomId));
-    console.log(`${playerName} created room ${roomId}`);
-  });
-
-  socket.on("joinRoom", ({ roomId, playerName, isHost }) => {
-    if (rooms.has(roomId)) {
-      const players = rooms.get(roomId);
-      players.push({ playerName, isHost });
-      rooms.set(roomId, players);
+io.on(
+  "connection",
+  (socket: Socket<ClientToServerEvents, ServerToClientEvents>) => {
+    socket.on("createRoom", ({ playerName, isHost }) => {
+      const roomId = uuidv4();
+      rooms.set(roomId, [{ playerName, isHost, socketId: socket.id }]);
       socket.join(roomId);
-      io.to(roomId).emit("updatePlayers", players);
-      console.log(`${playerName} joined room ${roomId}`);
-    }
-  });
-
-  socket.on("disconnect", () => {
-    rooms.forEach((players, roomId) => {
-      if (players.length === 0) {
-        rooms.delete(roomId);
+      io.to(roomId).emit("roomCreated", { roomId });
+      const players = rooms.get(roomId);
+      if (players) {
+        io.to(roomId).emit("updatePlayers", players);
+        console.log(`${playerName} created room ${roomId}`);
       }
     });
-  });
-});
+
+    socket.on("joinRoom", ({ roomId, playerName, isHost }) => {
+      if (rooms.has(roomId)) {
+        const players = rooms.get(roomId);
+        if (players) {
+          players.push({ playerName, isHost, socketId: socket.id });
+          rooms.set(roomId, players);
+          socket.join(roomId);
+          io.to(roomId).emit("updatePlayers", players);
+          console.log(`${playerName} joined room ${roomId}`);
+        }
+      }
+    });
+
+    socket.on("kickPlayer", ({ roomId, playerName }) => {
+      if (rooms.has(roomId)) {
+        const players = rooms.get(roomId);
+        if (players) {
+          const kickedPlayerSocketId = players.find(
+            (player) => player.playerName === playerName
+          )?.socketId;
+          const updatedPlayers = players.filter(
+            (player) => player.playerName !== playerName
+          );
+          rooms.set(roomId, updatedPlayers);
+          io.to(roomId).emit("updatePlayers", updatedPlayers);
+
+          if (kickedPlayerSocketId) {
+            io.to(kickedPlayerSocketId).emit("kicked", { playerName });
+          }
+        }
+      }
+    });
+
+    socket.on("disconnect", () => {
+      rooms.forEach((players, roomId) => {
+        if (players.length === 0) {
+          rooms.delete(roomId);
+        }
+      });
+    });
+  }
+);
 
 httpServer.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
