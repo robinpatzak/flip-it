a17798ce254abc509ac7cbe555b25985582886e6
Robin Patzak <robin.patzak@proton.me>
2025-01-02 17:55:00 +0100

    Add createRoom and joinRoom logic

---
+++ index.ts
@@ -0,0 +1,48 @@
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import { v4 as uuidv4 } from "uuid";
+
+const PORT = 4000;
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: { origin: "http://localhost:5173" },
+});
+
+const rooms = new Map();
+
+io.on("connection", (socket) => {
+  socket.on("createRoom", ({ playerName }) => {
+    const roomId = uuidv4();
+    rooms.set(roomId, [playerName]);
+    socket.join(roomId);
+    io.to(roomId).emit("roomCreated", { roomId });
+    io.to(roomId).emit("updatePlayers", rooms.get(roomId));
+    console.log(`${playerName} created room ${roomId}`);
+  });
+
+  socket.on("joinRoom", ({ roomId, playerName }) => {
+    if (rooms.has(roomId)) {
+      const players = rooms.get(roomId);
+      players.push(playerName);
+      rooms.set(roomId, players);
+      socket.join(roomId);
+      io.to(roomId).emit("updatePlayers", players);
+      console.log(`${playerName} joined room ${roomId}`);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    rooms.forEach((players, roomId) => {
+      if (players.length === 0) {
+        rooms.delete(roomId);
+      }
+    });
+  });
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
